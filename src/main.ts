311c2d33fe84fb9923fd742a1002fb4ebc62fea3
nicolatoscan <nicolatoscan99@outlook.it>
2021-04-15 11:04:49 +0200

    apollo proof of concept

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { createApp } from 'vue';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core';
+import { DefaultApolloClient } from '@vue/apollo-composable';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+const httpLink = createHttpLink({ uri: 'http://localhost:3000/graphql' });
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+createApp(App)
+  .provide(DefaultApolloClient, apolloClient)
+  .use(store)
+  .use(router)
+  .mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,24 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { gql } from 'graphql-tag';
+import { useQuery } from '@vue/apollo-composable';
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+  },
+  setup() {
+    const { result, error } = useQuery(gql`
+    query abc {
+      allPosts {
+        id
+      }
+    }
+    `);
+    console.log(result);
+    console.log(error);
   },
 });
 </script>
